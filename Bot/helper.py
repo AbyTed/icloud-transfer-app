b0a2b12030280531a21965d09e562605db2b912b
AbyTed <119811665+AbyTed@users.noreply.github.com>
2025-02-02 14:58:27 -0500

    Update helper.py

--- Bot/helper.py
+++ Bot/helper.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
-import timeweg
+import time
 
 
 import os
